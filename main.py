ebd4d8415eadab84711abb16641894228e6445f3
Jax <jaxdrummond9@gmail.com>
2023-07-06 13:44:37 -0400

    misc: moved canvas stuff to its own class

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-from tkinter import filedialog, colorchooser
-from tkinter import *
-
+from utils.canvas_manager import Canvas_Manager
 from utils.component import ComponentFactory
-from PIL import ImageGrab, Image, ImageTk
 
 
 class App:
@@ -28,90 +25,21 @@ class App:
         Loads the main components of the app.
         """
         resolution = (900, 900)
-        pixel_size = 14.0625
-        current_action = []
-        action_length = []
         canvas = ComponentFactory.create_canvas(self.window, resolution)
         canvas.pack()
+        canvas_object = Canvas_Manager(canvas)
 
-        def draw_pixel(event):
-            x = event.x // pixel_size
-            y = event.y // pixel_size
-            rect = canvas.create_rectangle(x * pixel_size, y * pixel_size, (x + 1) * pixel_size, (y + 1) * pixel_size,
-                                           fill=self.selected_colour, outline=self.selected_colour)
-            current_action.append(rect)
-
-
-        def erase_pixel(event):
-            x = event.x // pixel_size
-            y = event.y // pixel_size
-            canvas.create_rectangle(x * pixel_size, y * pixel_size, (x + 1) * pixel_size, (y + 1) * pixel_size,
-                                    outline="white", fill="white")
-
-        def on_draw_finish(e):
-            self.cached_actions.extend(current_action)
-            action_length.append(len(current_action))
-            current_action.clear()
-
-        def undo(e):
-            last_action_length = action_length[-1]
-            action_start = len(self.cached_actions) - last_action_length
-            action_end = len(self.cached_actions)
-            for action in self.cached_actions[action_start:action_end]:
-                canvas.delete(action)
-                self.cached_actions.remove(action)
-            action_length.remove(last_action_length)
-
-        canvas.bind("<B1-Motion>", draw_pixel)
-        self.window.bind("<ButtonRelease-1>", on_draw_finish)
-        canvas.bind("<B3-Motion>", erase_pixel)
-        self.window.bind("<Control-z>", undo)
-
-        def clear_canvas():
-            canvas.delete("all")
-
-        clear_button = ComponentFactory.create_button(self.window, "Clear", command=clear_canvas)
+        clear_button = ComponentFactory.create_button(self.window, "Clear", command=canvas_object.clear)
         clear_button.pack()
 
-        def change_colour():
-            color = colorchooser.askcolor()
-            self.selected_colour = color[1]
-
-        change_color_button = ComponentFactory.create_button(self.window, "Change Colour", command=change_colour)
+        change_color_button = ComponentFactory.create_button(self.window, "Change Colour", command=canvas_object.change_colour)
         change_color_button.pack()
 
-        def save_image():
-            # Get the dimensions of the canvas
-            x = canvas.winfo_rootx()
-            y = canvas.winfo_rooty()
-            width = canvas.winfo_width()
-            height = canvas.winfo_height()
-
-            # Capture a screenshot of the canvas
-            screenshot = ImageGrab.grab((x, y, x + width, y + height))
-
-            # Ask user for the save location and filename
-            filename = filedialog.asksaveasfilename(defaultextension=".png",
-                                                    filetypes=[("PNG Image", "*.png"), ("All Files", "*.*")])
-            if filename:
-                # Save the screenshot as an image file
-                screenshot.save(filename)
-                print("Image saved successfully.")
-
-        save_button = ComponentFactory.create_button(self.window, text="Save Image", command=save_image)
+        save_button = ComponentFactory.create_button(self.window, text="Save Image", command=canvas_object.save_image)
         save_button.pack()
 
-        def open_image():
-            file = filedialog.askopenfile(defaultextension=".png", filetypes=[("PNG Image", "*.png")])
-            if file:
-                image = (Image.open(file.name))
-                resized_image = image.resize(resolution, Image.LANCZOS)
-                new_image = ImageTk.PhotoImage(resized_image)
-                self.image = new_image
-                clear_canvas()
-                canvas.create_image((450, 450), image=self.image)
-
-        open_image_button = ComponentFactory.create_button(self.window, text="Open Image", command=open_image)
+        open_image_button = ComponentFactory.create_button(self.window, text="Open Image",
+                                                           command=canvas_object.open_image)
         open_image_button.pack()
 
 
---
+++ utils/canvas_manager.py
@@ -0,0 +1,117 @@
+from tkinter import filedialog, colorchooser
+from tkinter import *
+
+from PIL import ImageGrab, Image, ImageTk
+
+
+class Canvas_Manager:
+
+    def __init__(self, canvas: Canvas):
+        self.canvas = canvas
+        self.selected_colour = "black"
+        self.pixel_size = 14.0625
+        self.resolution = (900, 900)
+        self.cached_actions = []
+        self.current_action = []
+        self.action_length = []
+        canvas.bind("<B1-Motion>", self.draw_pixel)
+        canvas.master.bind("<ButtonRelease-1>", self.on_draw_finish)
+        canvas.master.bind("<ButtonRelease-3>", self.on_draw_finish)
+        canvas.bind("<B3-Motion>", self.erase_pixel)
+        canvas.master.bind("<Control-z>", self.undo)
+
+    def draw_pixel(self, event):
+        """
+        Draws a pixel on the canvas.
+        Is run when Mouse 1 is held and moved.
+
+        Args:
+            event: the event object
+
+        """
+
+        x = event.x // self.pixel_size
+        y = event.y // self.pixel_size
+        rect = self.canvas.create_rectangle(x * self.pixel_size, y * self.pixel_size, (x + 1) * self.pixel_size,
+                                            (y + 1) * self.pixel_size,
+                                            fill=self.selected_colour, outline=self.selected_colour)
+        self.current_action.append(rect)
+
+    def erase_pixel(self, event):
+        """Creates a white pixel"""
+
+        x = event.x // self.pixel_size
+        y = event.y // self.pixel_size
+        rect = self.canvas.create_rectangle(x * self.pixel_size, y * self.pixel_size, (x + 1) * self.pixel_size,
+                                            (y + 1) * self.pixel_size,
+                                            outline="white", fill="white")
+        self.current_action.append(rect)
+
+    def on_draw_finish(self, _):
+        """Called when drawing finishes. Adds the actions to cached_actions list."""
+        self.cached_actions.extend(self.current_action)
+        self.action_length.append(len(self.current_action))
+        self.current_action.clear()
+
+    def undo(self, _):
+        """Undoes the last action the user did."""
+        try:
+            last_action_length = self.action_length[-1]
+            action_start = len(self.cached_actions) - last_action_length
+            action_end = len(self.cached_actions)
+            for action in self.cached_actions[action_start:action_end]:
+                self.canvas.delete(action)
+                self.cached_actions.remove(action)
+            self.action_length.remove(last_action_length)
+        except IndexError:
+            print("No more actions to revert.")
+
+    def save_image(self):
+        """Saves the image on the canvas via ImageGrab"""
+        # Get the dimensions of the canvas
+        x = self.canvas.winfo_rootx()
+        y = self.canvas.winfo_rooty()
+        width = self.canvas.winfo_width()
+        height = self.canvas.winfo_height()
+
+        # Capture a screenshot of the canvas
+        screenshot = ImageGrab.grab((x, y, x + width, y + height))
+
+        # Ask user for the save location and filename
+        filename = filedialog.asksaveasfilename(defaultextension=".png",
+                                                filetypes=[("PNG Image", "*.png"), ("All Files", "*.*")])
+        if filename:
+            # Save the screenshot as an image file
+            screenshot.save(filename)
+            print("Image saved successfully.")
+
+    def open_image(self):
+        """Prompts the user to open a file via file explorer."""
+
+        # Asks to open file
+        file = filedialog.askopenfile(defaultextension=".png", filetypes=[("PNG Image", "*.png")])
+
+        # Checks if the user picked a file or not
+        if file:
+            # Opens and resizes image to canvas resolution
+            image = (Image.open(file.name))
+            resized_image = image.resize(self.resolution, Image.LANCZOS)
+            new_image = ImageTk.PhotoImage(resized_image)
+
+            # Adds the image to the object for memory
+            self.image = new_image
+
+            # Clears the canvas
+            self.clear()
+
+            # Creates an image on the canvas with the image the user opened
+            self.canvas.create_image((450, 450), image=self.image)
+
+    def clear(self):
+        """Clears the canvas"""
+        self.canvas.delete("all")
+
+    def change_colour(self):
+        """Sets the primary color to the choice of the user."""
+        color = colorchooser.askcolor()
+        self.selected_colour = color[1]
